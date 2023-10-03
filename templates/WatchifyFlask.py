fc728cbb8104c179b710dd1e2496105258d68690
Marcela Soriano Cornejo <marcelasorianoc@outlook.com>
2023-10-02 20:08:26 -0400

    Flask Updates

--- templates/WatchifyFlask.py
+++ templates/WatchifyFlask.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, request, session, url_for, render_template
-from os import environ  
+from os import environ
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -15,7 +15,7 @@ app.secret_key = environ.get('SECRET_KEY')
 # Set the Spotify API credentials
 client_id = environ.get('CLIENT_ID')
 client_secret = environ.get('CLIENT_SECRET')
-redirect_uri = environ.get('REDIRECT_URI')
+redirect_uri = 'http://localhost:3000/callback' 
 
 # Initialize Spotify API client
 sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='user-top-read')
@@ -27,9 +27,8 @@ def index():
         return redirect(url_for('loginpage'))
     token_info = session.get('token_info')
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    
+
     time_range = 'short_term'
-    limit = 10
     top_artists = sp.current_user_top_artists(time_range='short_term', limit=50)
 
     genre_count = {}
@@ -38,7 +37,7 @@ def index():
             genre_count[genre] = genre_count.get(genre, 0) + 1
 
     most_listened_genre = max(genre_count.keys(), key=lambda genre: genre_count[genre])
-    
+
     return f"The most listened genre over the past 30 days is: {most_listened_genre}"
 
 @app.route('/loginpage')
@@ -49,16 +48,14 @@ def loginpage():
 @app.route('/displayhistory')
 def display_history():
     if not session.get('token_info'):
-        # If the user is not logged in, redirect to the login page
-        return redirect(url_for('loginpage')) 
-    # Render the displayhistory.html template
+        return redirect(url_for('loginpage'))
     return render_template('displayhistory.html')
 
 @app.route('/callback')
 def callback():
     token_info = sp_oauth.get_access_token(request.args['code'], as_dict=False)
     session['token_info'] = token_info
-    # Redirect to the displayhistory route after obtaining the token information
+    # Redirect to the display_history route after obtaining the token information
     return redirect(url_for('display_history'))
 
 
