3067c9dcca7cc2d846afbb85f9a5f0f406d44002
Michael Dominguez <dmichael4952@gmail.com>
2023-10-10 00:24:50 -0400

    attempt 2

--- WatchifyFlask.py
+++ WatchifyFlask.py
@@ -15,7 +15,7 @@ app.secret_key = environ.get('SECRET_KEY')
 # Set the Spotify API credentials
 client_id = environ.get('CLIENT_ID')
 client_secret = environ.get('CLIENT_SECRET')
-# redirect_uri = 'https://shamp00the-cat.github.io/movierecs/callback' 
+redirect_uri = 'https://shamp00the-cat.github.io/movierecs/callback' 
 redirect_uri = 'http://127.0.0.1:5000/callback' 
 
 
