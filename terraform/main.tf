4b0eddadc12b3425213ff7c38b657f3f52f83016
Casey Roosma <casey.roosma75@myhunter.cuny.edu>
2023-10-11 14:21:07 -0400

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,41 @@
-resource "aws_vpc" "watchify" {
-  cidr_block = "10.0.0.0/16"
+#resource "aws_vpc" "watchify" {
+# cidr_block = "10.0.0.0/16"
+#}
+
+#terraform {
+# backend "s3" {
+#  bucket = "AWS_BUCKET_NAME"
+# key    = "AWS_BUCKET_KEY_NAME"
+#region = "AWS_REGION"
+#assume_role {
+#   role_arn = "AWS_ROLE"
+#     }
+# }
+#}
+
+resource "tls_private_key" "rsa-4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-terraform {
-  backend "s3" {
-    bucket = "AWS_BUCKET_NAME"
-    key    = "AWS_BUCKET_KEY_NAME"
-    region = "AWS_REGION"
-    role_arn = "AWS_ROLE"
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa-4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa-4096.private_key_pem
+  filename = var.key_name
+}
+
+variable "key_name" {}
+
+resource "aws_instance" "watchify" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "Watchify"
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,5 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIA3ZGLXBQVC3JU3V73"
+  secret_key = "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"
 }
