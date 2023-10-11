47409f1a5da495f88517457b0281847518fc0067
Marcela Soriano Cornejo <marcelasorianoc@outlook.com>
2023-10-11 17:55:31 -0400

    Flask Updates

--- WatchifyFlask.py
+++ WatchifyFlask.py
@@ -22,6 +22,8 @@ sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                         client_secret=SPOTIPY_CLIENT_SECRET,
                         redirect_uri=SPOTIPY_REDIRECT_URI,
                         scope=["user-top-read"])
+
+
 # DB Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@YOUR_RDS_ENDPOINT:5432/watchify' # change
 db = SQLAlchemy(app)
@@ -153,35 +155,35 @@ def index():
 def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
+    return render_template('loginpage.html')
 
 @app.route('/callback')
 def callback():
     code = request.args['code']
     token = sp_oauth.get_access_token(code, as_dict=False)
     session['token'] = token
-    return render_template('displayhistory.html')
+    return render_template('displayhistorpage.html')
 
 @app.route('/history')
 def display_history():
     # Fetch user's top tracks from the past 30 days (short term)
-    top_tracks = sp.current_user_top_tracks(limit=50, time_range='short_term')
-    # Extract artists from the top tracks
-    artist_ids = [track['album']['artists'][0]['id'] for track in top_tracks['items']]
-    # Fetch artist details for these artists
-    artists = sp.artists(artist_ids)['artists']
-    # Count frequency of each genre
-    genre_count = {}
-    for artist in artists:
-        for genre in artist['genres']:
-            if genre in genre_count:
-              genre_count[genre] += 1
-            else:
-              genre_count[genre] = 1
-
-    # Ensure that the genres from Spotify are in lowercase for the mapping.
-    top_5_genres = [genre.lower() for genre in sorted(genre_count, key=genre_count.get, reverse=True)[:5]]
-    print("Your top 5 most listened to genres over the past 30 days are: ("top_5_genres")")
-    return render_template('displayhistory.html', history=recommendations_db.to_dict(orient='records'))
+top_tracks = sp.current_user_top_tracks(limit=50, time_range='short_term')
+# Extract artists from the top tracks
+artist_ids = [track['album']['artists'][0]['id'] for track in top_tracks['items']]
+# Fetch artist details for these artists
+artists = sp.artists(artist_ids)['artists']
+# Count frequency of each genre
+genre_count = {}
+for artist in artists:
+    for genre in artist['genres']:
+        if genre in genre_count:
+            genre_count[genre] += 1
+        else:
+            genre_count[genre] = 1
+# Sort genres by frequency, get top 5, and capitalize the first letter of each genre
+top_5_genres = [genre.capitalize() for genre in sorted(genre_count, key=genre_count.get, reverse=True)[:5]]
+print("Your top 5 most listened to genres over the past 30 days are: ("top_5_genres")")
+return render_template('displayhistory.html', history=recommendations_db.to_dict(orient='records'))
 
 @app.route('/recommendation', methods=['POST'])
 def recommendation():
